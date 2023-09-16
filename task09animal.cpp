a89a7fcced6e581fd23b7509e101caf32e321a5b
Noor-e-Hira979 <144408192+Noor-e-Hira979@users.noreply.github.com>
2023-09-16 21:28:04 +0500

    Add files via upload

---
+++ task09animal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+main(){ 
+system("color 87");   
+cout<<"    -------------------------------   "<<endl;
+cout<<"                                      "<<endl;
+cout<<"     @          ^____^                "<<endl;
+cout<<"              @ ( O O )\\________      "<<endl;
+cout<<"                ( ___ )\\        )\\/\\ "<<endl;
+cout<<"                       | |----w |    "<<endl;
+cout<<"                                     "<<endl;
+cout<<"                       | |    | |    "<<endl;
+}
